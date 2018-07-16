3c6f14bfe3da786c262043fbbd38c353bbaa594e
jason escobar <jasonjavii09@gmail.com>
2018-07-16 11:35:26 -0600

    more fixx

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'biblioteca', 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
