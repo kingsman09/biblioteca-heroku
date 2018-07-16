33a5a46dd56506a0a995069b77e803bf88727672
jason escobar <jasonjavii09@gmail.com>
2018-07-16 11:46:43 -0600

    fixx

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'biblioteca', 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -142,4 +142,4 @@ LOGIN_REDIRECT_URL = 'usuarios:index'
 LOGOUT_REDIRECT_URL = 'usuarios:index'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
