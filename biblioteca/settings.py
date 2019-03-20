5ce2d2a90cd356878d776994b9cd433c1be0a518
Jason Escobar <jason_javier@live.com>
2019-03-20 15:41:58 -0600

    added cors

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'apps.prestamos',
 
     'import_export',
+    'corsheaders',
 ]
 
 IMPORT_EXPORT_TRANSACTIONS = True
@@ -59,7 +60,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'biblioteca.urls'
 
